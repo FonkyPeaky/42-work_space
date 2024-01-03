14488dae85ab400a79f6a7c9d670d19a0ba638d5
Iraunikel <unikelira@gmail.com>
2024-01-03 13:12:32 +0100

    ex06 edit, added ex07
    commented main func in ex06
    added ex07 and commented main func there too

--- Ira/00_Global/00_Piscine_Reloaded/ex07/ft_print_numbers.c
+++ Ira/00_Global/00_Piscine_Reloaded/ex07/ft_print_numbers.c
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: irinaineshina <iraunikel@student.42.lu>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/03 12:54:21 by irinaineshina     #+#    #+#             */
-/*   Updated: 2024/01/03 13:11:48 by irinaineshina    ###   ########.fr       */
+/*   Created: 2024/01/03 13:03:52 by irinaineshina     #+#    #+#             */
+/*   Updated: 2024/01/03 13:11:21 by irinaineshina    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
+
 void	ft_putchar(char i)
 {
 	write(1, &i, 1);
 }
 
-void	ft_print_alphabet(void)
+void	ft_print_numbers(void)
 {
 	char	i;
-	i = 'a';
-
-	while (i <= 'z')
+	i = '1';
+	while (i <= '9')
 	{
 		ft_putchar(i);
 		i++;
@@ -29,6 +29,8 @@ void	ft_print_alphabet(void)
 /*
 int	main(void)
 {
-	ft_print_alphabet();
+	ft_print_numbers();
 	return(0);
 }*/
+
+
